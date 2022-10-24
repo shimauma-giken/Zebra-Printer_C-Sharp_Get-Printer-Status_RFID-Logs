8b610121d19e2247d895103d39647466cdb2912c
Shimauma-Giken <zebra.share01@gmail.com>
2022-10-24 16:07:27 +0900

    add get-host-status

--- WForm_GetRfidLogsFromZebraPrinter01/WForm_GetRfidLogsFromZebraPrinter01/Form1.Designer.cs
+++ WForm_GetRfidLogsFromZebraPrinter01/WForm_GetRfidLogsFromZebraPrinter01/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -46,17 +50,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(18, 99);
+            this.button2.Location = new System.Drawing.Point(18, 77);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(161, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Enjoy RF logs!";
+            this.button2.Text = "Get RFID logs!";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(210, 48);
+            this.button3.Location = new System.Drawing.Point(18, 106);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(161, 47);
             this.button3.TabIndex = 2;
@@ -66,19 +70,63 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(411, 48);
+            this.button4.Location = new System.Drawing.Point(450, 48);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(161, 23);
+            this.button4.Size = new System.Drawing.Size(236, 23);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Get current status.";
+            this.button4.Text = "Printer Status - Get Current Stauts";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(450, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(236, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Get Host Status - get.host_status";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(197, 48);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(236, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Syslog - Erase Syslog Entires";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(197, 77);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(236, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Syslog - Enable Syslog";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(197, 106);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(236, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Syslog - Get Syslog Entries";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -95,5 +143,9 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
