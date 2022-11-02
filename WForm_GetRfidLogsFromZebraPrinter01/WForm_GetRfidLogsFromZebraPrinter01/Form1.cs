fe74d255264bbd7672d27aaf179b755928678bbd
Shimauma-Giken <zebra.share01@gmail.com>
2022-11-02 17:01:53 +0900

    Update Form1.cs

--- WForm_GetRfidLogsFromZebraPrinter01/WForm_GetRfidLogsFromZebraPrinter01/Form1.cs
+++ WForm_GetRfidLogsFromZebraPrinter01/WForm_GetRfidLogsFromZebraPrinter01/Form1.cs
@@ -9,7 +9,7 @@ namespace WForm_GetRfidLogsFromZebraPrinter01
 {
     public partial class Form1 : Form
     {
-        string ipAddress = "192.168.4.55";
+        string ipAddress = "192.168.4.54";
         string zplStrings = "^XA^RFR,H^XZ";
 
         public Form1()
@@ -55,6 +55,9 @@ namespace WForm_GetRfidLogsFromZebraPrinter01
             for (int i = 0; i< 10; i++)
             {
 
+                /*
+                // ZPL送信後に実施した方がパフォーマンスが良いため、移動。 
+                //
                 // Get Printer Status 
                 Debug.WriteLine(String.Format("{0:yyyy年MM月dd日(ddd) HH時mm分ss秒fff}", DateTime.Now) + " :Start getting printer status.");
                 while (true)
@@ -72,12 +75,12 @@ namespace WForm_GetRfidLogsFromZebraPrinter01
                         break;
                     }
                 }
+                */
 
 
 
-                zplStrings = "^XA^RFW,H^FD0000000000A" + i.ToString() + "^XZ";
-                // zplStrings = @"^XA^MMC^CWK,E:IPAG.TTF^FS^CI28^PW1146^LL319^LS0^FT21,136^AK,41,41^FD現^FS^FT21,184^AK,41,41^FD品^FS^FT21,231^AK,41,41^FD票^FS^FT165,68^AK,50,50^FDBXT155-38-10F-5C=1^FS^FT0,117^FB389,1,12,R^AK,45,45^FD20^FS^FT0,117^FB708,1,12,R^AK,45,45^FD20^FS^FT0,113^FB933,1,11,R^AK,41,41^FD1/1^FS^FT165,150^AK,29,29^FD次工程協力会社 スリーケ株^FS^FT248,209^AK,50,50^FD2022/05/24^FS^FT148,255^AK,41,41^FDマニホールドベース　^FS^FT164,195^FB982,1,7,C^AK,29,29^FDＡＳ^FS^FT148,291^AK,33,33^FD02001 飯塚鉄工（株）^FS^FT791,291^AK,33,33^FD519999635^FS^FT774,190^AK,25,25^FD^FS^FT644,242^AK,29,29^FD023AAC62-66^FS^FT0,60^FB1121,1,11,R^AK,41,41^FD2^FS^FT0,92^FB1121,1,8,R^AK,33,33^FD8461^FS^FT992,234^BQN,2,4^FH\^FDMA,BXT155-38-10F-5C=1$0000020$003020012209051519999635010001$^FS^FO602,159^GB106,47,5^FS^FT709,291^AK,33,33^FD注番^FS^FT709,194^AK,29,29^FD手配^FS^FT0,60^FB1068,1,11,R^AK,41,41^FD設変^FS^FT573,246^AK,33,33^FD工号^FS^FT71,246^AK,33,33^FD品名^FS^FT71,291^AK,33,33^FD社名^FS^FT71,198^AK,33,33^FD納入日^FS^FT71,154^AK,33,33^FD場所^FS^FT71,111^AK,37,37^FD数量^FS^FT71,56^AK,37,37^FD品番^FS^FT957,295^AK,58,58^FDH^FS^FT988,282^FB158,1,7,C^AK,29,29^FDＳＭＣ^FS^FO951,245^GB41,51,5^FS^FT969,84^AK,25,25^FD請求元^FS^FT933,84^AK,25,25^FD袋^FS^FT933,114^AK,25,25^FD箱^FS^FT390,112^AK,33,33^FD個^FS^FT709,112^AK,33,33^FD個^FS^FT449,112^AK,33,33^FD計^FS^LRY^FO21,53^GB0,224,41^FS^LRN^LRY^FO1013,249^GB94,0,44^FS^LRN^PQ1,,,Y^RFW,H^FD534d43001efe909301000001^FS^XZ";
-                //Debug.WriteLine(zplStrings);
+                zplStrings = "^XA^RFW,H^FD00000000000" + i.ToString() + "^XZ";
+                Debug.WriteLine(zplStrings);
 
                 Debug.WriteLine(String.Format("{0:yyyy年MM月dd日(ddd) HH時mm分ss秒fff}", DateTime.Now) + " :Label #" + i);
 
@@ -87,6 +90,23 @@ namespace WForm_GetRfidLogsFromZebraPrinter01
                 Debug.WriteLine(String.Format("{0:yyyy年MM月dd日(ddd) HH時mm分ss秒fff}", DateTime.Now) + " :Send ZPL.");
                 SendZplOverTcp(ipAddress, zplStrings);
 
+                // Get Printer Status 
+                Debug.WriteLine(String.Format("{0:yyyy年MM月dd日(ddd) HH時mm分ss秒fff}", DateTime.Now) + " :Start getting printer status.");
+                while (true)
+                {
+                    Thread.Sleep(100);
+                    str = SgdOverTcp_GetPrinterStatus(ipAddress);
+                    //Debug.WriteLine($"Length is -- > {str.IndexOf(t)}");
+                    if (str != null)
+                    {
+                        Debug.WriteLine(String.Format("{0:yyyy年MM月dd日(ddd) HH時mm分ss秒fff}", DateTime.Now) + " :Recieved printer status.");
+                        Debug.WriteLine($"Printer Status is -- > {str}");
+
+                        // Write Error check code Here!
+
+                        break;
+                    }
+                }
 
 
 
